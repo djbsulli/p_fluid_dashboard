322cb16c5af5b203ffb24c6d452397ab1b64c1dd
djbsulli <dannyjamesbryansullivan@gmail.com>
2026-04-04 21:56:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -459,7 +459,7 @@ elif page == "Player Statistics":
         st.markdown("---")
 
         # ── Similar Players ───────────────────────────────────────────────────
-       st.markdown(f'Most Similar Players By Positional Fluidity: {selected_position} (All Leagues)')
+        st.markdown(f'Most Similar Players By Positional Fluidity: {selected_position} (All Leagues)')
 
         all_pos_players = season_player_stats[
             (season_player_stats['position_cat'] == selected_position) &
