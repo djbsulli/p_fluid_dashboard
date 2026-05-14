70e5ca95086f9a410f62cae072b0c1ba6b444906
djbsulli <dannyjamesbryansullivan@gmail.com>
2026-05-14 21:17:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -680,7 +680,7 @@ elif page == "Player Statistics":
 
         # Similar players (by z-score distance)
         st.markdown(
-            f'<div class="section-header">Most Similar {selected_position}s</div>',
+            f'<div class="section-header">Most Similar Players in Position div>',
             unsafe_allow_html=True
         )
 
