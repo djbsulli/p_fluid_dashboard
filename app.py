a81b0b67efca7b8a05a050b3a1f934dd1815cca4
djbsulli <dannyjamesbryansullivan@gmail.com>
2026-05-19 19:16:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -203,7 +203,7 @@ st.markdown(f"""
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Google Drive ID for the large touches parquet file
-TOUCHES_GDRIVE_ID = "1b--N4X8tDqvNDK0M4nNyEKohyEXgu2a6"
+TOUCHES_GDRIVE_ID = "1CWAHjO_Cxvq_e-aSFXp9XFFarF0LVNYC"
 
 
 @st.cache_data
